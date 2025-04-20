32ec4eef78be24c45fbeb004e17533921a7e1f67
claxten10 <claxten10@gmail.com>
2025-04-20 22:12:58 +0200

    vienna: Configure Platform and Bootloader flags
    Signed-off-by: claxten10 <claxten10@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,5 +12,12 @@ TARGET_ARCH_VARIANT := armv8-2a-dotprod
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_VARIANT := cortex-a78
 
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := vienna
+TARGET_NO_BOOTLOADER := true
+
+# Platform
+TARGET_BOARD_PLATFORM := mt6878
+
 # Inherit the proprietary files
 include vendor/motorola/vienna/BoardConfigVendor.mk
