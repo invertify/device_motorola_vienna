28997889df55dd428760243e35502cc71c0e96e3
sabrina010209 <sebidk700@gmail.com>
2025-07-01 18:58:52 -0500

    vienna: Initial skeleton tree
    Change-Id: Iee29710a456ed2912996c88ca4c929e1327eccf2

---
+++ BoardConfig.mk
@@ -0,0 +1,10 @@
+#
+# Copyright (C) The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+DEVICE_PATH := device/motorola/vienna
+
+# Inherit the proprietary files
+include vendor/motorola/vienna/BoardConfigVendor.mk
---
+++ lineage_vienna.mk
@@ -0,0 +1,30 @@
+#
+# SPDX-FileCopyrightText: LineageOS
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from device makefile.
+$(call inherit-product, device/motorola/vienna/device.mk)
+
+# Inherit some common lineageOS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+TARGET_BOOT_ANIMATION_RES := 1080
+
+PRODUCT_NAME := lineage_vienna
+PRODUCT_DEVICE := vienna
+PRODUCT_MANUFACTURER := Motorola
+PRODUCT_BRAND := motorola
+PRODUCT_MODEL := motorola edge 50 neo
+
+PRODUCT_GMS_CLIENTID_BASE := android-motorola
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    DeviceName=vienna \
+    BuildDesc="vienna_g_sys-user 16 W1UIS36M.39-17-1 43a25 release-keys" \
+    BuildFingerprint=motorola/vienna_g_hal/vienna:14/W1UIS36H.39-17-1/82d4dc:user/release-keys
