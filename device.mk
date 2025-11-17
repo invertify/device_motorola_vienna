7bddc7c52c9b327104aa09c2c65d97db4e992056
claxten10 <claxten10@gmail.com>
2025-11-17 22:49:55 +0000

    vienna: Build USB and Gadget service
    Change-Id: Id7aad09e0a72691a35461d07821090fdd5b477af
    Signed-off-by: claxten10 <claxten10@gmail.com>
    (cherry picked from commit 2d4d3166293926254c626cfbd9540d2045536c23)

--- device.mk
+++ device.mk
@@ -137,6 +137,15 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.keystore.app_attest_key.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.keystore.app_attest_key.xml \
     frameworks/native/data/etc/android.software.device_id_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_id_attestation.xml
 
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb-service.mediatek \
+    android.hardware.usb.gadget-service.mediatek
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
+    frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(DEVICE_PATH) \
